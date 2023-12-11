a42113e924e27ed04c725e886dbb6ddfaf4b82e7
rasakumar neduncheralathan <57787319+rneduncheralathan@users.noreply.github.com>
2023-12-11 11:54:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload,Jenkins and docker container deployment...
         </p>
         <a
           className="App-link"
